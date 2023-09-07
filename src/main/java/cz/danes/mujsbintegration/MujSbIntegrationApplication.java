d5a862ddab8bfbb148f9edc9ab5c4f0d44b84e1b
“DZCM4G4” <danielvacha.dv@gmail.com>
2023-09-07 21:36:43 +0200

    Initial commit

---
+++ src/main/java/cz/danes/mujsbintegration/MujSbIntegrationApplication.java
@@ -0,0 +1,13 @@
+package cz.danes.mujsbintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujSbIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MujSbIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/danes/mujsbintegration/MujSbIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.danes.mujsbintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujSbIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
