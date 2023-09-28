e60d6197bfee086ff7435813913f570c586999da
“DZCM4G4” <danielvacha.dv@gmail.com>
2023-09-28 14:34:50 +0200

    remove non routers file

--- src/main/java/cz/danes/mujsbintegration/tcp/MessageDeserializingException.java
+++
@@ -1,8 +0,0 @@
-package cz.danes.mujsbintegration.tcp;
-
-public class MessageDeserializingException extends RuntimeException{
-
-    public MessageDeserializingException() {
-        super("Error while deserializing message");
-    }
-}
--- src/main/java/cz/danes/mujsbintegration/tcp/TcpMessageReceived.java
+++
@@ -1,7 +0,0 @@
-package cz.danes.mujsbintegration.tcp;
-
-public interface TcpMessageReceived {
-    String TCP_MESSAGE_RECEIVED_SUBSCRIBABLE_CHANNEL_NAME = "car-received-subscribable-channel-name";
-
-    void onCarReceivedEvent(String message);
-}
